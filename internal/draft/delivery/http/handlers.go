856f96c74f67b75b0be99826321f4f3675ea1aeb
Mikhail Parfenov <vgbhjob@gmail.com>
2026-02-03 18:34:00 +0300

    impl delivery http

--- internal/draft/delivery/http/handlers.go
+++ internal/draft/delivery/http/handlers.go
@@ -1,1 +1,36 @@
 package http
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/vgbhj/draftdle/internal/draft"
+)
+
+type DraftHandler struct {
+	draftUC draft.DraftUC
+}
+
+func NewDraftHandler(uc draft.DraftUC) draft.Handler {
+	return &DraftHandler{
+		draftUC: uc,
+	}
+}
+
+func (d *DraftHandler) Get() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		draft, err := d.draftUC.GetRandomDraft()
+		if err != nil {
+
+			return c.JSON(http.StatusInternalServerError, struct {
+				Message string `json:"message"`
+				Status  int    `json:"status"`
+			}{
+				Message: "Cant get draft",
+				Status:  http.StatusInternalServerError,
+			})
+		}
+
+		return c.JSON(http.StatusOK, draft)
+	}
+}
